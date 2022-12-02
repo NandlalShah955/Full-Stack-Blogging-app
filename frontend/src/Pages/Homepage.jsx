1b7e27df330de6560a0d0ebf6d024edec77de253
Nandlal <shahnandlal49@gmail.com>
2022-12-02 15:39:02 +0530

    adding

--- frontend/src/Pages/Homepage.jsx
+++ frontend/src/Pages/Homepage.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import axios from 'axios'
 
 function Homepage() {
+  const [posts, setposts] = useState([])
+  const [loading, setloading] = useState(false)
+
+  useEffect(() => {
+  const loadpost=async()=>{
+ setloading(false)
+ const res=await axios.get(
+  "http://localhost:8080/posts"
+ )
+ setposts(res.data)
+console.log(res)
+ setloading(false)
+
+  }
+loadpost()
+
+  }, [])
+  
+  
+  
+  
+  
+  
   return (
-    <div>Homepage</div>
+    <div>
+
+
+
+
+    </div>
   )
 }
 
