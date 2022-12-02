f3febd5417b590a0035c5bd181a1f8efba505e35
Nandlal <shahnandlal49@gmail.com>
2022-12-02 16:27:14 +0530

    homepage done

--- backend/src/features/posts/post.route.js
+++ backend/src/features/posts/post.route.js
@@ -8,50 +8,50 @@ app.get("/",async(req,res)=>{
     res.send(posts)
 })
 
-app.post("/upload",async(req,res)=>{
-try {
-    let posts=await Post.create({
-        ...req.body,
-    })
-    res.send(posts)
-} catch (e) {
-    res.status(404).send(e.message)
-}
-})
+// app.post("/upload",async(req,res)=>{
+// try {
+//     let posts=await Post.create({
+//         ...req.body,
+//     })
+//     res.send(posts)
+// } catch (e) {
+//     res.status(404).send(e.message)
+// }
+// })
 
-app.patch("/:id/updatepost",async(req,res)=>{
-let id = req.params.id
-try {
-    let posts=await Post.findByIdAndUpdate(
-        id,
-        {
-            ...req.body
+// app.patch("/:id/updatepost",async(req,res)=>{
+// let id = req.params.id
+// try {
+//     let posts=await Post.findByIdAndUpdate(
+//         id,
+//         {
+//             ...req.body
 
-    },
-    {
-        new: true,
-    }
+//     },
+//     {
+//         new: true,
+//     }
     
-    )
-    res.send(posts,'Post updated Successfully')
-} catch (e) {
-    res.status(404).send(e.message)
-}
-})
+//     )
+//     res.send(posts,'Post updated Successfully')
+// } catch (e) {
+//     res.status(404).send(e.message)
+// }
+// })
 
-app.delete("/:id/deletepost",async(req,res)=>{
-let id= req.params.id
-try {
-    let posts=await Post.findByIdAndDelete(id)
-    if(posts){
-res.send("Post Deleted Successfully")
-    }else{
-        res.send("Cannot delete non-existing post")
-    }
-} catch (e) {
-    res.status(404).send(e.message)
-}
-})
+// app.delete("/:id/deletepost",async(req,res)=>{
+// let id= req.params.id
+// try {
+//     let posts=await Post.findByIdAndDelete(id)
+//     if(posts){
+// res.send("Post Deleted Successfully")
+//     }else{
+//         res.send("Cannot delete non-existing post")
+//     }
+// } catch (e) {
+//     res.status(404).send(e.message)
+// }
+// })
 
 
 
--- frontend/src/Pages/Homepage.jsx
+++ frontend/src/Pages/Homepage.jsx
@@ -1,38 +1,44 @@
-import React,{useState,useEffect} from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Stack, SimpleGrid, Image, Text, VStack } from "@chakra-ui/react";
 
 function Homepage() {
-  const [posts, setposts] = useState([])
-  const [loading, setloading] = useState(false)
+  const [posts, setposts] = useState([]);
+  const [loading, setloading] = useState(false);
 
   useEffect(() => {
-  const loadpost=async()=>{
- setloading(false)
- const res=await axios.get(
-  "http://localhost:8080/posts"
- )
- setposts(res.data)
-console.log(res)
- setloading(false)
+    const loadpost = async () => {
+      setloading(false);
+      const res = await axios.get("http://localhost:8080/posts");
 
+      setposts(res.data);
+      // console.log(res.data)
+      // console.log('post',posts)
+      setloading(false);
+    };
+    loadpost();
+  }, []);
+
+  if (loading) {
+    return <>..loading</>;
+  } else {
+    return (
+      <Stack bg='purple.100'>
+       <Text fontSize='4xl' as='citi'>Blogopedia</Text>
+       
+        {posts?.map((el) => (
+          <SimpleGrid key={el.id} columns={[1, 1, 2, 2]} bg='red.100' mt='20px'>
+            <Image boxSize="500px" marginLeft="100px" src={el.image}></Image>
+            <VStack margin='40px'>
+               <Text fontSize="4xl">{el.title}</Text>
+            <Text as='cite'>{el.description}</Text>
+            </VStack>
+           
+          </SimpleGrid>
+        ))}
+      </Stack>
+    );
   }
-loadpost()
-
-  }, [])
-  
-  
-  
-  
-  
-  
-  return (
-    <div>
-
-
-
-
-    </div>
-  )
 }
 
-export default Homepage
+export default Homepage;
