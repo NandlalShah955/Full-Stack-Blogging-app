b43f5398b4a5d5dcfd3b5401e7eed8f9d25acada
Nandlal <shahnandlal49@gmail.com>
2022-12-04 18:20:17 +0530

    added admin context private route

--- frontend/src/Components/Privateroute.js
+++ frontend/src/Components/Privateroute.js
@@ -1,28 +1,27 @@
-import React ,{useContext}from 'react'
-import{Navigate} from 'react-router-dom'
-import {Alert,AlertIcon} from '@chakra-ui/react'
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { Alert, AlertIcon } from "@chakra-ui/react";
 
-import {AuthContext} from '../Context/AuthContext'
-function Privateroute({children}) {
-  const {authState}=useContext(AuthContext)
-  
+import { AuthContext } from "../Context/AuthContext";
+function Privateroute({ children }) {
+  const navi = useNavigate();
+  const { authState } = useContext(AuthContext);
 
-  if(!authState.isAuth){
-    return( <>
-    <Navigate to='/'></Navigate>
-
-    <Alert status='warning'>
-    <AlertIcon />
-    Seems like you are not admin
-  </Alert>
-    </>
-    ) 
-   
+  if (!authState.isAuth) {
+    return (
+      <div>
+        <Alert status="warning">
+          <AlertIcon />
+          Seems like you are not admin
+        </Alert>
+        {setTimeout(() => {
+          navi("/");
+        }, 2000)}
+      </div>
+    );
   }
-  
-    return children
-   
-  
+
+  return children;
 }
 
-export default Privateroute
+export default Privateroute;
--- frontend/src/Pages/Allroutes.jsx
+++ frontend/src/Pages/Allroutes.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
-import {Routes,Route} from 'react-router-dom'
-import Login from './Login'
-import Signup from './Signup'
-import Homepage from './Homepage'
-import AdminPanel from './AdminPanel'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "./Login";
+import Signup from "./Signup";
+import Homepage from "./Homepage";
+import Privateroute from "../Components/Privateroute";
+import AdminPanel from "./AdminPanel";
 function Allroutes() {
   return (
     <Routes>
-    <Route path='/admin' element={<AdminPanel/>}></Route>
-      <Route path='/' element={<Homepage/>}></Route>
-      <Route path='/signup' element={<Signup/>}></Route>
-      <Route path='/login' element={<Login/>}></Route>
+      <Route
+        path="/admin"
+        element={
+          <Privateroute>
+            <AdminPanel />
+          </Privateroute>
+        }
+      ></Route>
+      <Route path="/" element={<Homepage />}></Route>
+      <Route path="/signup" element={<Signup />}></Route>
+      <Route path="/login" element={<Login />}></Route>
     </Routes>
-  )
+  );
 }
 
-export default Allroutes
+export default Allroutes;
