cfd4d13df5b40a422bdc7a208143b30e838d3553
Nandlal <shahnandlal49@gmail.com>
2022-12-04 16:21:57 +0530

    adding

--- backend/src/routes/posts/post.route.js
+++ backend/src/routes/posts/post.route.js
@@ -1,4 +1,4 @@
-const Post=require("./post.model")
+const Post=require("../../models/post.model")
 const express= require("express")
 const app=express.Router()
 
--- backend/src/routes/user/user.route.js
+++ backend/src/routes/user/user.route.js
@@ -1,4 +1,4 @@
-const User = require("./user.model");
+const User = require("../../models/user.model");
 const express = require("express");
 const app = express.Router();
 const jwt = require("jsonwebtoken");
